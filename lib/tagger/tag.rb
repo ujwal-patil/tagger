0070403adfd47dd60471c3cd2fbf0aa6ab3c4ff0
Ujwal Patil <ujwal@promobitech.com>
2020-05-04 18:34:01 +0530

    Tagger - Adding file_path issue fix

--- lib/tagger/tag.rb
+++ lib/tagger/tag.rb
@@ -2,14 +2,14 @@ module Tagger
 	class Tag
 		def initialize(file_path, instance)
 			@instance = instance
-			file_name = file_path.remove("#{instance.tags_directory}/")
+			file_name = file_path.gsub("#{instance.tags_directory}/", '')
 			_name = file_name.split('.')
 
-    	@file_path = file_path,
-    	@original = file_name,
-    	@name = _name.first,
-    	@created_at = _name.second.to_i,
-    	@hexdigest = _name.third,
+    	@file_path = file_path
+    	@original = file_name
+    	@name = _name.first
+    	@created_at = _name.second.to_i
+    	@hexdigest = _name.third
     	@locale = _name.second_to_last
     	@file_type = _name.last
 		end
@@ -17,6 +17,7 @@ module Tagger
 		attr_reader :file_path, :instance, :original, :name, :created_at, :hexdigest, :locale, :file_type
 
 		def remove
+			puts "\n\e[31mRemoving tag file: #{file_path}\e[0m"
 			FileUtils.rm_f(file_path)
 		end
 	end
